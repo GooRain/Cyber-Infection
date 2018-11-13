ffd625c6347cc5b5320559e48c6f7ddf9308b332
poewmuh <grandow78@gmail.com>
2018-11-13 20:51:57 +0900

    EnemySpawnSimpleCode
    навешать на генерируемый объект красного цвета

---
+++ Cyber Infection/Assets/Scripts/GameMechanic/Spawn/EnemySpawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public EnemysPool Pool { get; set; }
+
+    void Start ()
+    {
+        StartCoroutine(SpawnControl());
+    }
+
+    private void Awake()
+    {
+        Pool = GetComponent<EnemysPool>();
+
+    }
+
+    private IEnumerator SpawnControl()
+    {
+        int monsterIndex = Random.Range(0, 4);
+        string type = string.Empty;
+        switch (monsterIndex)
+        {
+            case 0:
+                type = "blue_enemy";
+                break;
+            case 1:
+                type = "orange_enemy";
+                break;
+            case 2:
+                type = "purple_enemy";
+                break;
+            case 3:
+                type = "red_enemy";
+                break;
+        }
+
+        Pool.GetObject(type);
+        yield return new WaitForSeconds(1.0f);
+    }
+}
