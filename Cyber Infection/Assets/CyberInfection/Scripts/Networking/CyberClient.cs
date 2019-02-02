75c06ab1f3b9b018a3dc9a587f012ab7668028ca
Vitcor <reegorz@gmail.com>
2019-02-03 01:08:31 +0900

    - Photon joining to room

---
+++ Cyber Infection/Assets/CyberInfection/Scripts/Networking/CyberClient.cs
@@ -0,0 +1,21 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace CyberInfection.Networking
+{
+    public class CyberClient : LoadBalancingClient
+    {
+        public void CallConnect()
+        {
+            AppId = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime;  // set your app id here
+            AppVersion = PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion;  // set your app version here
+
+            if (!ConnectToRegionMaster("rue")) // can return false for errors
+            {
+                DebugReturn(DebugLevel.ERROR, "Can't connect to: " + this.CurrentServerAddress);
+            }
+        }
+    }
+}
--- Cyber Infection/Assets/CyberInfection/Scripts/Networking/NetworkManager.cs
+++ Cyber Infection/Assets/CyberInfection/Scripts/Networking/NetworkManager.cs
@@ -1,17 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 
 namespace CyberInfection.Networking
 {
-    public class NetworkManager : MonoBehaviour
+    public class NetworkManager : MonoBehaviourPunCallbacks
     {
+        public static NetworkManager instance { get; private set; }
+
+        public static CyberClient client { get; private set; }
+
         private void Awake()
         {
-            
+            if (instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            PhotonNetwork.ConnectUsingSettings();
         }
 
-        public void ConnectTo(string ip)
+        public override void OnConnectedToMaster()
         {
-            
+            Debug.Log("OnConnectedToMaster");
+            var roomOptions = new RoomOptions {IsVisible = false, MaxPlayers = 4};
+            var creatingRoom = PhotonNetwork.JoinOrCreateRoom("Testing", roomOptions, TypedLobby.Default);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Joined!");
         }
     }
 }
