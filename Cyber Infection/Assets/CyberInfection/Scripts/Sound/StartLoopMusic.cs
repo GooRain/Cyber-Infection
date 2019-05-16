47e6dcb39983c5bf543caf0b498e0c8495437ba2
Vitcor <reegorz@gmail.com>
2019-05-17 04:02:20 +0900

    - Good loop music

---
+++ Cyber Infection/Assets/CyberInfection/Scripts/Sound/StartLoopMusic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace CyberInfection.Sound
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class StartLoopMusic : MonoBehaviour
+    {
+        [SerializeField] private AudioClip startClip;
+        [SerializeField] private AudioClip loopClip;
+        
+        private AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            PlayStart();
+
+            Invoke(nameof(PlayLoop), startClip.length);
+        }
+
+        private void PlayStart()
+        {
+            audioSource.clip = startClip;
+            audioSource.loop = false;
+            audioSource.Play();
+        }
+
+        private void PlayLoop()
+        {
+            audioSource.clip = loopClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
